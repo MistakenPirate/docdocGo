25c5d3a669f972f3eeccbc89601a58f6128ede34
sam <mistakenpirate38@gmail.com>
2024-08-13 21:28:31 +0530

    ai on halt

--- my-app/src/components/drawer-ai.tsx
+++ my-app/src/components/drawer-ai.tsx
@@ -12,7 +12,7 @@ import {
   DrawerTrigger,
 } from "./ui/drawer";
 import { Button } from "./ui/button";
-import { openAI } from "@/utils/open-ai";
+// import { openAI } from "@/utils/open-ai";
 import { Loader } from "lucide-react";
 
 interface DrawerProps {
@@ -21,28 +21,28 @@ interface DrawerProps {
 
 const DrawerAI  = ({description}: DrawerProps) => {
   const [open, setOpen] = useState(false);
-  const [wizardSuggestion, setWizardSuggestion] = useState("");
-  const [isLoading, setIsLoading] = useState(false);
+  // const [wizardSuggestion, setWizardSuggestion] = useState("");
+  // const [isLoading, setIsLoading] = useState(false);
 
-  const handleWizardSuggestion = async () => {
-    setIsLoading(true);
-    try {
-      const response = (await openAI(description!)) as string;
-      setWizardSuggestion(response);
-      setIsLoading(false);
-    } catch (error) {
-      console.log(error);
-    }
-  };
+  // const handleWizardSuggestion = async () => {
+  //   setIsLoading(true);
+  //   try {
+  //     const response = (await openAI(description!)) as string;
+  //     setWizardSuggestion(response);
+  //     setIsLoading(false);
+  //   } catch (error) {
+  //     console.log(error);
+  //   }
+  // };
 
-  console.log(wizardSuggestion);
+  // console.log(wizardSuggestion);
   return (
     <div>
       <Drawer open={open} onOpenChange={setOpen}>
         <DrawerTrigger className="flex float-right">
-          <Button variant={"outline"} onClick={handleWizardSuggestion}>
+          {/* <Button variant={"outline"} onClick={handleWizardSuggestion}>
             Ask your wizard 🧞
-          </Button>
+          </Button> */}
         </DrawerTrigger>
         <DrawerContent>
           <DrawerHeader>
@@ -50,13 +50,13 @@ const DrawerAI  = ({description}: DrawerProps) => {
               Hi there, with my magic spell, I&apos;`ll help you tell your
               story. AbraCadabraAllakazum
             </DrawerTitle>
-            {isLoading ? (
+            {/* {isLoading ? (
               <Loader className="flex mx-auto justify-center animate-spin" />
             ) : (
               <DrawerDescription className="whitespace-pre-wrap">
                 {wizardSuggestion.length > 0 && <p>{wizardSuggestion}</p>}
               </DrawerDescription>
-            )}
+            )} */}
           </DrawerHeader>
         </DrawerContent>
       </Drawer>
--- my-app/src/components/editor-block.tsx
+++ my-app/src/components/editor-block.tsx
@@ -76,7 +76,7 @@ const EditorBlock: React.FC<EditorBlockProps> = ({ document }) => {
   return (
     <div className="px-4">
       <div className="flex float-right my-2 space-x-4">
-        <DrawerAI description={document.description} />
+        {/* <DrawerAI description={document.description} /> */}
         <form onSubmit={onDocumentDelete} className="flex float-right">
           <Button type="submit" variant={"destructive"}>
             Delete
